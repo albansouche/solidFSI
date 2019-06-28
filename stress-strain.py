3247edf9985b0380436aa2bb216702c9f08d0189
Syver Døving Agdestein <syverda@icloud.com>
2019-06-28 14:41:22 +0200

    Modularized code to allow for smoother change of parameters.

--- stress-strain.py
+++ stress-strain.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
+
+# Inter- and extrapolator
 def extrap1d(interpolator):
     xs = interpolator.x
     ys = interpolator.y
@@ -16,49 +18,62 @@ def extrap1d(interpolator):
 
     return pointwise
 
+### PARAMETERS ################################################################
+
 folder = 'results/law/'
+extension = '.out'
 
-eps_lin, sig_lin = np.loadtxt(folder+'LinearElastic.out')
-eps_SVK, sig_SVK = np.loadtxt(folder+'StVenantKirchhoff.out')
-#eps_neo, sig_neo = np.loadtxt(folder+'neoHookean.out')
+models = ['LinearElastic', 'StVenantKirchhoff']
+#models = ['LinearElastic', 'StVenantKirchhoff', 'neoHookean']
+N_models = len(models)
 
+#xlabel = 'Infinitesimal volumetric Strain $\\operatorname{tr}(\\epsilon)$'
+xlabel = 'Infinitesimal Strain $\\epsilon_{zz}$'
+#xlabel = 'Green-Lagrange Strain $E_{zz}$'
+#xlabel = 'Norm of displacement'
 
-SVKint = interp1d(eps_SVK, sig_SVK)
-SVK = np.vectorize(extrap1d(SVKint))
-err_SVK = np.abs(SVK(eps_lin)-sig_lin)
-relerr_SVK = err_SVK[1:]/sig_lin[1:]
+ylabel = '$\\operatorname{tr}(\\sigma)$'
+#ylabel = '$\\sigma_{zz}$'
+#ylabel = '$Von Mises stress'
 
-"""
-neoint = interp1d(eps_neo, sig_neo)
-neo = np.vectorize(extrap1d(neoint))
-err_neo = np.abs(neo(eps_lin)-sig_lin)
-relerr_neo = err_neo[1:]/sig_lin[1:]
-"""
+###############################################################################
 
-plt.figure(figsize=[15, 11])
-plt.plot(eps_lin, sig_lin, label='Linear Elastic')
-#plt.plot(eps_lin, neo(eps_lin), label='SVK interp')
-plt.plot(eps_SVK, sig_SVK, label='StVenant-Kirchhoff')
-#plt.plot(eps_neo, sig_neo, label='Neo-Hookean')
-#plt.xlabel('Green-Lagrange Strain $E_{zz}$')
-#plt.xlabel('Infinitesimal Strain $\\epsilon_{zz}$')
-plt.xlabel('Norm of displacment')
-plt.ylabel('$\\operatorname{tr}(\\sigma)$')
-#plt.ylabel('$\\sigma_{zz}$')
+eps = [0] * N_models
+sig = [0] * N_models
+relation_point = [0] * N_models
+relation = [0] * N_models
+err = [0] * (N_models-1)
+relerr = [0] * (N_models-1)
+
+# Extract data
+for i in range(N_models):
+    eps[i], sig[i] = np.loadtxt(folder+models[i]+extension)
+    relation_point[i] = interp1d(eps[i], sig[i])
+    relation[i] = np.vectorize(extrap1d(relation_point[i]))
+
+# Difference between linear model (0) and the others (1->N_models)
+for i in range(1, N_models):
+    err[i-1] = np.abs(relation[i](eps[0])-sig[0])
+    relerr[i-1] = err[i-1][1:]/sig[0][1:]
+
+# Plot stress-strain relation for all models
+plt.figure()
+for i in range(N_models):
+    #if i >= 1: plt.plot(eps[0], relation[i](eps[0]), label=models[i]+' interpolated')
+    plt.plot(eps[i], sig[i], label=models[i])
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
 plt.title('Stress-strain in the point $(0.11, 0.0, 0.0)$')
 plt.legend()
 plt.savefig(folder+'stress-strain.png')
 
 
-
-
-plt.figure(figsize=[15, 11])
-plt.plot(eps_lin[1:], relerr_SVK, label='StVenantKirchhoff')
-#plt.plot(eps_lin[1:], relerr_neo, label='Neo-Hookean')
-#plt.xlabel('Infinitesimal Strain $\\epsilon_{zz}$')
-plt.xlabel('Norm of displacment')
-plt.ylabel('$\\operatorname{tr}(\\sigma)$')
-#plt.ylabel('$rel error \\sigma_{zz}$')
-plt.title('Error: $\\frac{hyper-lin}{lin}$')
-#plt.legend()
+# Plot relative difference between linear model and the others
+plt.figure()
+for i in range(1, N_models):
+    plt.plot(eps[0][1:], relerr[i-1], label=models[i])
+plt.xlabel(xlabel)
+plt.ylabel('Relative error of '+ylabel)
+plt.title('Relative difference: $\\frac{y_{hyper}-y_{lin}}{y_{lin}}$')
+plt.legend()
 plt.savefig(folder+'error.png')
