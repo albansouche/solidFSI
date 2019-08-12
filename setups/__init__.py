6d7d0b9a583556627885e717834fb49bc5c796b9
Syver Døving Agdestein <syverda@icloud.com>
2019-08-12 08:49:27 +0200

    Initialize quantities so that they do not have to be specified if assumed zero.

--- setups/__init__.py
+++ setups/__init__.py
@@ -2,6 +2,17 @@
 
 class Setup_base():
 
+    def __init__(self):
+
+        # Quantities assumed to be zero if not specified
+        self.p_exp = []
+        self.prestress = []
+        self.body_force = []
+        self.u0 = []
+        self.v0 = []
+        self.extension = ""
+        self.obs_points = []
+
     def reshape_parent_mesh(self, **namespace):
         """
         """
